c5443342f3065ee67cb23b6d47be7bb867b03669
Sebastien Tremblay-Johnston <stremblay-johnston@sparkgeo.com>
2025-10-07 10:21:35 -0700

    formatting

--- prescient_sdk/upload.py
+++ prescient_sdk/upload.py
@@ -85,7 +85,7 @@ def upload(
         input_dir (str | os.PathLike): Input directory containing file(s) to be uploaded.
             By default will upload all files contained in input directory. This can be an
             absolute or relative path, the final path component will be included as part
-            of the object key e.g. /path/to/data_dir -> s3://bucket/data_dir/file.txt. 
+            of the object key e.g. /path/to/data_dir -> s3://bucket/data_dir/file.txt.
             When input_dir is a relative path, this should be relative to the current working
             directory used to execute this function.
         exclude (Optional[list[str]]): A list of glob patterns to exclude from uploading.
